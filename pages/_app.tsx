1c81b8a3a120e55c4fb2d11d1e9dc39aa6ef5726
Douglas Bernardes <douglaspb96@gmail.com>
2021-06-01 02:24:33 -0300

    Add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,24 @@
 import Head from "next/head";
 import ThemeContainer from "../contexts/theme/ThemeContainer";
-import Router from 'next/router';
-import NProgress from 'nprogress';
+import Router from "next/router";
+import NProgress from "nprogress";
 
-Router.events.on('routeChangeStart', (url) => {
-  NProgress.start()
-})
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+Router.events.on("routeChangeStart", (url) => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeContainer>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        
+
+        <title>Stokei</title>
+
+        <link rel="shortcut icon" href="/favicon.png" />
+
         {/* Import CSS for nprogress */}
         <link rel="stylesheet" type="text/css" href="/nprogress.css" />
       </Head>
