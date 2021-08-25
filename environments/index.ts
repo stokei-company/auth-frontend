1665e80f0f68d9eab5adaf48f68bd78fc6fb5bdf
Stokei Oficial <stokeioficial@gmail.com>
2021-08-25 03:56:40 -0300

    Remove redirect

--- environments/index.ts
+++ environments/index.ts
@@ -1,5 +1,4 @@
-export const isProduction: boolean = process.env.NODE_ENV === 'production';
+export const isProduction: boolean = process.env.NODE_ENV === "production";
 
 export const AUTH_SERVER_URL: string = process.env.NEXT_PUBLIC_AUTH_SERVER_URL;
-export const PAINEL_URL: string = process.env.NEXT_PUBLIC_PAINEL_URL;
-export const PAINEL_ADMIN_URL: string = process.env.NEXT_PUBLIC_PAINEL_ADMIN_URL;
+export const DASHBOARD_URL: string = process.env.NEXT_PUBLIC_DASHBOARD_URL;
