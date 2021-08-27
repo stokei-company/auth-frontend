8e9189ee9111b7b061d35c678dbbf72d70240a7a
stokei <stokei@users.noreply.github.com>
2021-08-27 04:26:19 +0000

    Prettified Code!

--- config/axios.ts
+++ config/axios.ts
@@ -4,39 +4,38 @@ import { authHeader } from '~/utils/auth';
 import { AUTH_SERVER_URL } from '../environments';
 
 export const axiosClient = axios.create({
-    baseURL: AUTH_SERVER_URL,
-    headers: {
-        "Content-type": "application/json",
-        ...authHeader(),
-    }
+  baseURL: AUTH_SERVER_URL,
+  headers: {
+    'Content-type': 'application/json',
+    ...authHeader()
+  }
 });
 
-interface RequestProps extends AxiosRequestConfig {
-}
+interface RequestProps extends AxiosRequestConfig {}
 
 interface RequestPayload<TData = any> {
-    readonly data: TData;
-    readonly error: any;
+  readonly data: TData;
+  readonly error: any;
 }
 
 export function useRequest(props: RequestProps): RequestPayload[] {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await axiosClient(props);
-                if (response && response.data) {
-                    setData(response.data);
-                } else {
-                    setData(null);
-                }
-            } catch (error) {
-                setError(error);
-            }
-        })()
-    }, []);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axiosClient(props);
+        if (response && response.data) {
+          setData(response.data);
+        } else {
+          setData(null);
+        }
+      } catch (error) {
+        setError(error);
+      }
+    })();
+  }, []);
 
-    return [{ data, error }];
+  return [{ data, error }];
 }
--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -1,43 +1,45 @@
-import { currentDevice } from "~/services/device";
-import { AUTH_SERVER_URL } from "../../environments";
-import { mountUri } from "../uri/mount-uri";
+import { currentDevice } from '~/services/device';
+import { AUTH_SERVER_URL } from '../../environments';
+import { mountUri } from '../uri/mount-uri';
 
-export const SITE_NAME = "Stokei";
+export const SITE_NAME = 'Stokei';
 
-export const logoUrl = "https://assets.stokei.com/logos/logo.png";
-export const faviconUrl = "https://assets.stokei.com/logos/favicon.png";
+export const logoUrl = 'https://assets.stokei.com/logos/logo.png';
+export const faviconUrl = 'https://assets.stokei.com/logos/favicon.png';
 
 interface ParamsSocialMediaURI {
-    readonly redirectUri?: string;
-    readonly appId?: string;
+  readonly redirectUri?: string;
+  readonly appId?: string;
 }
 
 const authURI = async (baseURL: string, data: ParamsSocialMediaURI) => {
-    const device = currentDevice();
-    return await mountUri(baseURL, [
-        data.redirectUri && {
-            key: "redirectUri",
-            value: data.redirectUri,
-        },
-        data.appId && {
-            key: "appId",
-            value: data.appId,
-        },
-        device && {
-            key: "device",
-            value: JSON.stringify(device),
-        },
-    ]);
-}
+  const device = currentDevice();
+  return await mountUri(baseURL, [
+    data.redirectUri && {
+      key: 'redirectUri',
+      value: data.redirectUri
+    },
+    data.appId && {
+      key: 'appId',
+      value: data.appId
+    },
+    device && {
+      key: 'device',
+      value: JSON.stringify(device)
+    }
+  ]);
+};
 
-export const googleAuthURI = (data: ParamsSocialMediaURI) => authURI(AUTH_SERVER_URL + "/google", data);
+export const googleAuthURI = (data: ParamsSocialMediaURI) =>
+  authURI(AUTH_SERVER_URL + '/google', data);
 
-export const facebookAuthURI = (data: ParamsSocialMediaURI) => authURI(AUTH_SERVER_URL + "/facebook", data);
+export const facebookAuthURI = (data: ParamsSocialMediaURI) =>
+  authURI(AUTH_SERVER_URL + '/facebook', data);
 
 export const colors = {
-    primary: {
-        dark: "green.700",
-        main: "green.600",
-        light: "green.500",
-    }
+  primary: {
+    dark: 'green.700',
+    main: 'green.600',
+    light: 'green.500'
+  }
 };
